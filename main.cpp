1c028bf1da56020ff86ee5beb2237871cb5f4b92
vkashkin <vlad.kashkin2014@yandex.ru>
2022-03-11 00:16:10 +0400

    Функция вычисляется итеративно, выводит первые n чисел Фибоначчи

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 using namespace std;
 long fib(int n) {
-if (n <= 0) 
-    return -1;
-if (n == 1) 
-    return 0;
-if (n == 2) 
-    return 1;
-return fib(n - 1) + fib(n - 2);
+	if (n <= 0) 
+       return -1;
+	if (n >= 1) {
+		cout << "0" << endl;
+	}
+	if (n >= 2) {
+		cout << "1" << endl;
+	}
+	long r = 0, a = 1;
+	for (int i = 2; i < n; ++i) {
+		long dev = r + a;
+		r = a;
+		a = dev;
+		cout << a << endl;
+	}
+	return a;
 }
 int main() {
-cout << fib(10);
-}
+	fib(10);
+} 
