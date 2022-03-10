abfb2ffef18d2bc28ad724268c682c725709b201
vkashkin <vlad.kashkin2014@yandex.ru>
2022-03-11 00:13:14 +0400

    Функция fib(n) теперь начинается с нуля

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 long fib(int n) {
-	if (n <= 0) 
-        return -1;
-	if (n == 1 or n == 2) 
-       return 1;
-	return fib(n - 1) + fib(n - 2);
+if (n <= 0) 
+    return -1;
+if (n == 1) 
+    return 0;
+if (n == 2) 
+    return 1;
+return fib(n - 1) + fib(n - 2);
 }
 int main() {
-	cout << fib(10);
-} 
+cout << fib(10);
+}
